0d0374e8280c3582444f9e21552086766ead2900
AGR <abhishekgrevankar@gmail.com>
2024-08-24 19:33:11 +0530

    LinkedList - Get method completed, start with Set.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
+import java.net.StandardSocketOptions;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(1);
+        LinkedList myLinkedList = new LinkedList(7);
         myLinkedList.append(2);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+        myLinkedList.append(6);
 
         // (2) Items - Returns 2 Node
-        System.out.println(myLinkedList.removeLast().value);
+        // System.out.println(myLinkedList.removeLast().value);
         // (1) Item - Returns 1 Node
-        System.out.println(myLinkedList.removeLast().value);
+       //  System.out.println(myLinkedList.removeLast().value);
         // (0) Items - Returns null
-        System.out.println(myLinkedList.removeLast());
-        System.out.println(myLinkedList.prepend(8).value);
+        // System.out.println(myLinkedList.removeLast());
+         System.out.println(myLinkedList.prepend(8).value);
+        // myLinkedList.removeFirst();
+        myLinkedList.printList();
+        System.out.println();
+        //System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.get(2).value + "\n");
 
     }
         
